c7826c3aa388d180619cac6ec2da6162fa2bcc90
Mormyshev <mormyshevoleg@gmail.com>
2026-02-08 08:48:39 +0300

    fix: pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import HomePage from "@/pages/home/HomePage";
+import { Header } from "@/widgets/header";
+import { SearchBar } from "@/widgets/add-bar";
+import { TodoList } from "@/widgets/todo-list";
 
 export default function Home() {
   return (
     <main className="w-full min-h-screen flex flex-col items-center">
-      <HomePage />
+      <div className="max-w-[420px] w-full mt-16 flex flex-col items-center">
+        <Header />
+        <SearchBar />
+        <TodoList />
+      </div>
     </main>
   );
 }
